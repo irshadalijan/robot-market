c809e67ecb6c2e3537faff957fe4a00a880485a4
Irshad Ali Jan <hunger_of_success@hotmail.com>
2021-12-03 17:27:34 +0500

    Install NextJs and setup tailwind css for the project

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-screen h-screen bg-grey-200 flex items-center justify-center text-3xl">
+      Welcome to Robots Market
+    </div>
+  );
+};
+
+export default Home;
